2514f0bbded83073f6c4175ab163972464257c15
Vision295 <78678991+Vision295@users.noreply.github.com>
2023-03-16 15:33:54 +0100

    Highlight system done
    grow the size of different keys needed to be inputed

--- TestUnityProject/Assets/_Scripts/KeyboardGame/ManagerGame.cs
+++ TestUnityProject/Assets/_Scripts/KeyboardGame/ManagerGame.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class ManagerGame : MonoBehaviour
 {
@@ -7,14 +8,11 @@ public class ManagerGame : MonoBehaviour
     private char[] example = "Ceci est le premier exemple tu dois essayer d'écrire ce qui est affiché. Bon courage !".ToCharArray();
     // string later converted into a chain of character
     public string display = "Ceci est le premier ";
+    public GameObject[] keyboard;
 
     // advancement in the example (index of the character are we at)
     int advancement = 0;
     TMP_Text text;
-    void Awake()
-    {
-        text = GetComponent<TMP_Text>();
-    }
 
     string FillDisplay(char[] _display, char _nextCharacter)
     {
@@ -37,6 +35,72 @@ public class ManagerGame : MonoBehaviour
 
         return new string(m_display);
     }
+
+    void HighLight(char _nextCharacter)
+    {
+        /*
+            function used to highlight (make it bigger) the key associated to the 
+            character (_nextCharacter) supposed to be written
+        */
+
+        // to exclude the name of certain objects
+        bool error;
+
+        // loop throw keyboard to find the corresponding key
+        foreach(GameObject item in keyboard)
+        {
+            // loop throw different rows of keys (AKeys, WKeys, ...)
+            foreach(RectTransform children in item.GetComponentsInChildren<RectTransform>())
+            {
+                // exclude the name of the rows (AKeys, WKeys, ...) and maj
+                switch(children.name)
+                {
+                    case "AKeys":
+                        error = true; break;
+                    case "QKeys":
+                        error = true; break;
+                    case "WKeys":
+                        error = true; break;
+                    case "NumberKeys":
+                        error = true; break;
+                    case "MAJ":
+                        error = true; break;
+                    default:
+                        error = false; break;
+                }
+
+                // if the key isn't an excluded one
+                if(!error)
+                {
+                    // find _nextCharacter in the name of the different keys
+                    foreach(char a in children.name.ToCharArray())
+                    {
+                        // if all conditions required are forfilled than we change 
+                        // the scale of the chosen key
+                        if(_nextCharacter == a)
+                        {
+                            children.localScale = new Vector3(2, 2, 1);
+                            // we break to avoid reducing the size of the key to  (1, 1, 1) 
+                            // if there is another character in the name of the key
+                            break;
+                        } else
+                        {
+                            children.localScale = new Vector3(1, 1, 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+    void Start()
+    {
+        HighLight(example[advancement]);
+    }
     
     void Update()
     {
@@ -52,6 +116,7 @@ public class ManagerGame : MonoBehaviour
                     advancement++;
                     // display : string --> char[]
                     display = FillDisplay(display.ToCharArray(), example[display.Length + advancement - 1]);
+                    HighLight(example[advancement]);
                 } 
             }
             // same : if index not out of range
@@ -62,6 +127,7 @@ public class ManagerGame : MonoBehaviour
                     advancement++;
                     // display : string --> char[]
                     display = FillDisplay(display.ToCharArray(), ' ');
+                    HighLight(example[advancement]);
                 } 
             }
         }
